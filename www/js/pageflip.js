13306764e4d62ae4ea302790aab7eef27569be10
Alexey Kisel <aki@jetthoughts.com>
2012-01-12 23:47:58 +0200

    fix pagination

--- www/js/pageflip.js
+++ www/js/pageflip.js
@@ -68,13 +68,17 @@
 	}
 	
 	function mouseDownHandler( event ) {
+        mouseMoveHandler(event);
 		// Make sure the mouse pointer is inside of the book
 		if (Math.abs(mouse.x) < PAGE_WIDTH) {
 			if (mouse.x < 0 && page - 1 >= 0) {
 				// We are on the left side, drag the previous page
 				flips[page - 1].dragging = true;
 			}
-			else if (mouse.x > 0 && page + 1 < flips.length) {
+			else if (mouse.x > 0 && (page < flips.length-1)) {
+
+                console.log(page);
+                                console.log(flips.length);
 				// We are on the right side, drag the current page
 				flips[page].dragging = true;
 			}
@@ -91,7 +95,7 @@
 				// Figure out which page we should navigate to
 				if( mouse.x < 0 ) {
 					flips[i].target = -1;
-					page = Math.min( page + 1, flips.length );
+					page = Math.min( page + 1, flips.length-1 );
 				}
 				else {
 					flips[i].target = 1;
@@ -154,13 +158,14 @@
 		context.save();
 		context.translate( CANVAS_PADDING + ( BOOK_WIDTH / 2 ), PAGE_Y + CANVAS_PADDING );
 		
+        var dFoldX = foldX - 35;
 		
 		// Draw a sharp shadow on the left side of the page
 		context.strokeStyle = 'rgba(0,0,0,'+(0.05 * strength)+')';
 		context.lineWidth = 30 * strength;
 		context.beginPath();
-		context.moveTo(foldX - foldWidth, -verticalOutdent * 0.5);
-		context.lineTo(foldX - foldWidth, PAGE_HEIGHT + (verticalOutdent * 0.5));
+		context.moveTo(dFoldX - foldWidth, -verticalOutdent * 0.5);
+		context.lineTo(dFoldX - foldWidth, PAGE_HEIGHT*0.75 + (verticalOutdent * 0.5));
 		context.stroke();
 		
 		
@@ -171,10 +176,10 @@
 		
 		context.fillStyle = rightShadowGradient;
 		context.beginPath();
-		context.moveTo(foldX, 0);
-		context.lineTo(foldX + rightShadowWidth, 0);
-		context.lineTo(foldX + rightShadowWidth, PAGE_HEIGHT);
-		context.lineTo(foldX, PAGE_HEIGHT);
+		context.moveTo(dFoldX , 0);
+		context.lineTo(dFoldX + rightShadowWidth, 0);
+		context.lineTo(dFoldX  + rightShadowWidth, PAGE_HEIGHT*0.75);
+		context.lineTo(dFoldX , PAGE_HEIGHT*0.75);
 		context.fill();
 		
 		
@@ -185,33 +190,35 @@
 		
 		context.fillStyle = leftShadowGradient;
 		context.beginPath();
-		context.moveTo(foldX - foldWidth - leftShadowWidth, 0);
-		context.lineTo(foldX - foldWidth, 0);
-		context.lineTo(foldX - foldWidth, PAGE_HEIGHT);
-		context.lineTo(foldX - foldWidth - leftShadowWidth, PAGE_HEIGHT);
+		context.moveTo(dFoldX - foldWidth - leftShadowWidth, 0);
+		context.lineTo(dFoldX- foldWidth, 0);
+		context.lineTo(dFoldX- foldWidth, PAGE_HEIGHT*0.75);
+		context.lineTo(dFoldX- foldWidth - leftShadowWidth, PAGE_HEIGHT*0.75);
 		context.fill();
 		
 		
 		// Gradient applied to the folded paper (highlights & shadows)
-		var foldGradient = context.createLinearGradient(foldX - paperShadowWidth, 0, foldX, 0);
+		var foldGradient = context.createLinearGradient(dFoldX - paperShadowWidth, 0, dFoldX, 0);
 		foldGradient.addColorStop(0.35, '#fafafa');
 		foldGradient.addColorStop(0.73, '#eeeeee');
 		foldGradient.addColorStop(0.9, '#fafafa');
 		foldGradient.addColorStop(1.0, '#e2e2e2');
 		
 		context.fillStyle = foldGradient;
-		context.strokeStyle = 'rgba(0,0,0,0.06)';
+		context.strokeStyle = 'rgba(0,0,0,0.6)';
 		context.lineWidth = 0.5;
 		
 		// Draw the folded piece of paper
 		context.beginPath();
-		context.moveTo(foldX, 0);
-		context.lineTo(foldX, PAGE_HEIGHT);
-		context.quadraticCurveTo(foldX, PAGE_HEIGHT + (verticalOutdent * 2), foldX - foldWidth, PAGE_HEIGHT + verticalOutdent);
-		context.lineTo(foldX - foldWidth, -verticalOutdent);
-		context.quadraticCurveTo(foldX, -verticalOutdent * 2, foldX, 0);
+		context.moveTo(dFoldX, 0);
+		context.lineTo(dFoldX, PAGE_HEIGHT);
+        
+		context.quadraticCurveTo(dFoldX, PAGE_HEIGHT*0.75 + (verticalOutdent * 2), dFoldX - foldWidth, PAGE_HEIGHT*0.75 + verticalOutdent);
+		context.lineTo(dFoldX- foldWidth+10, -verticalOutdent);
+		context.quadraticCurveTo(dFoldX, -verticalOutdent * 2, dFoldX, 0);
 		
 		context.fill();
+        
 		context.stroke();
 		
 		
