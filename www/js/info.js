0695f6ebd4d8ed314a69fb5a035e78488b926d0e
Alexey Kisel <aki@jetthoughts.com>
2012-01-17 16:51:15 +0200

    text in center. bugfix

--- www/js/info.js
+++ www/js/info.js
@@ -1,81 +1,93 @@
+var DEFAULT_Y = 80;
+var DEFAULT_X = 0;
+
 TEXTS = [
- [
-  [{text: "Book 1 Page 1 Text Line 1", x:5, y:75, time: 3},
-   
-   {text: "Book 1 Page 1 Text Line 2", x:25, y:75, time: 6},
-   {text: "Book 1 Page 1 Text Line 3",x:25, y:75, time:10},
-   {text: "Book 1 Page 1 Text Line 4",x:25, y:75, time:15}
-   ],
-   
-   [
-    {text:  "Book 1 Page 2 Text Line 1", x:20, y:60},
-    {text:  "Book 1 Page 2 Text Line 2", x:20, y:60}
+    [
+        [
+            {text:"Book 1 Page 1 Text Line 1", x:5, y:75, time:3},
+
+            {text:"Book 1 Page 1 Text Line 2", x:25, y:75, time:6},
+            {text:"Book 1 Page 1 Text Line 3", x:25, y:75, time:10},
+            {text:"Book 1 Page 1 Text Line 4", x:25, y:75, time:15}
+        ],
+
+        [
+            {text:"Book 1 Page 2 Text Line 1", x:20, y:60},
+            {text:"Book 1 Page 2 Text Line 2", x:20, y:60}
+        ]
     ],
- ],
-         /* Story 2*/
-         [
-          [{text: "At 8 o'clock Sock's house gets dark", x:12, y:70, time:4},
-           
-           
-           {text: "The birds don't sing, and the dogs don't bark", x:12, y:70, time:11},
-           {text: "<b>The</b>he whole neigborhood turns out their lights,",x:12, y:70, time:13},
-           {text: "and they all go to sleep at night.",x:12, y:70, time:15}
-           ],
-         
-          [{text: "Tick..tock...tick...tock.", x:12, y:70,time:5.5},
-           
-           {text: "The clock strikes 8 and its bedtime for Sock", x:12, y:70,time:10},
-           {text: "He'll get ready for bed, then he'll turn out his light,",x:12, y:70,time:14},
-           {text: "but Sock is afraid of the dark at night",x:12, y:70, time:19}
-           ],
-          
-          [{text: "He pulls his covers up to his head,", x:12, y:70,time:2},
-           {text: "and lies there shaking in his bed.", x:12, y:70, time:6},
-           {text: "\"I'm scared, I'm scared\" Sock begins to shout.",x:12, y:70, time:12},
-           {text: "\"I don't like when the lights go out\"",x:12, y:70, time:16}
-           ],
-          
-          [{text: "Sock doesn't like to turn out the light,", x:12, y:70,time:3},
-           
-           {text: "because the house makes scary noise at night.", x:12, y:70, time:7},
-           {text: "Now it's dark inside Sock's room,",x:12, y:70, time:11},
-           {text: "except for the light from the shining moon",x:12, y:70, time:14}
-           ],
-          
-          [{text: "Squeak.....squeak....... goes the floor.", x:12, y:70, time:4},
-           
-           {text: "Is that a noise coming from the closet door?", x:12, y:70, time:7},
-           {text: "The very thought gives Sock a really big scare.",x:12, y:70, time:11},
-           {text: "Could it be something hiding in there?",x:12, y:70, time:15}
-           ],
-        
-          
-          [{text: "Sock says, \"there's a monster in my room,", x:12, y:70,time:2},
-           
-           {text: "and I know he'll try to eat me soon.\"", x:12, y:70,time:6},
-           {text: "All is quiet from celing to floor.",x:12, y:70,time:11},
-           {text: "Is there really a monster behind the door?",x:12, y:70,time:15}
-           ],
-          
-          [{text: "Sock is as curious as can be,", x:12, y:70,time:2},
-           
-           {text: "so he hops out of bed to go and see.", x:12, y:70,time:5},
-           {text: "He opens the close and turns on the light,",x:12, y:70,time:10},
-           {text: "and there are no monsters anywhere in sight.",x:12, y:70,time:20}
-           ],
-          
-          [{text: "Just to be sure Sock looks all around,", x:12, y:70,time:5},
-           
-           {text: "then he hops back in bed to sleep safe and sound.", x:12, y:70,time:10},
-           {text: "\"I'm glad I checked for myself,\" says Sock.",x:12, y:70, time:13},
-           {text: "\"Now I won't be afraid when it turns 8 o'clock. \"",x:12, y:70, time:17}
-           ],
-          
-          [{text: "So when Sock is afraid of the dark at night,", x:12, y:70, time:3},
-           
-           {text: "he just checks his whole room when he turns on the light.", x:12, y:70, time:7},
-           {text: "Now Sock can sleep without a care,",x:12, y:70, time:11},
-           {text: "because he knows there are no monsters there.",x:12, y:70, time:17}
-           ]
-          ]
+    /* Story 2*/
+    [
+        [
+            {text:"At 8 o'clock Sock's house gets dark", x:DEFAULT_X, y:DEFAULT_Y, time:4},
+
+            {text:"The birds don't sing, and the dogs don't bark", x:DEFAULT_X, y:DEFAULT_Y, time:11},
+            {text:"<b>T</b>he whole neigborhood turns out their lights,", x:DEFAULT_X, y:DEFAULT_Y, time:13},
+            {text:"and they all go to sleep at night.", x:DEFAULT_X, y:DEFAULT_Y, time:15}
+        ],
+
+        [
+            {text:"Tick..tock...tick...tock.", x:DEFAULT_X, y:DEFAULT_Y, time:5.5},
+
+            {text:"The clock strikes 8 and its bedtime for Sock", x:DEFAULT_X, y:DEFAULT_Y, time:10},
+            {text:"He'll get ready for bed, then he'll turn out his light,", x:DEFAULT_X, y:DEFAULT_Y, time:14},
+            {text:"but Sock is afraid of the dark at night", x:DEFAULT_X, y:DEFAULT_Y, time:19}
+        ],
+
+        [
+            {text:"He pulls his covers up to his head,", x:DEFAULT_X, y:DEFAULT_Y, time:2},
+            {text:"and lies there shaking in his bed.", x:DEFAULT_X, y:DEFAULT_Y, time:6},
+            {text:"\"I'm scared, I'm scared\" Sock begins to shout.", x:DEFAULT_X, y:DEFAULT_Y, time:12},
+            {text:"\"I don't like when the lights go out\"", x:DEFAULT_X, y:DEFAULT_Y, time:16}
+        ],
+
+        [
+            {text:"Sock doesn't like to turn out the light,", x:DEFAULT_X, y:DEFAULT_Y, time:3},
+
+            {text:"because the house makes scary noise at night.", x:DEFAULT_X, y:DEFAULT_Y, time:7},
+            {text:"Now it's dark inside Sock's room,", x:DEFAULT_X, y:DEFAULT_Y, time:11},
+            {text:"except for the light from the shining moon", x:DEFAULT_X, y:DEFAULT_Y, time:14}
+        ],
+
+        [
+            {text:"Squeak.....squeak....... goes the floor.", x:DEFAULT_X, y:DEFAULT_Y, time:4},
+
+            {text:"Is that a noise coming from the closet door?", x:DEFAULT_X, y:DEFAULT_Y, time:7},
+            {text:"The very thought gives Sock a really big scare.", x:DEFAULT_X, y:DEFAULT_Y, time:11},
+            {text:"Could it be something hiding in there?", x:DEFAULT_X, y:DEFAULT_Y, time:15}
+        ],
+
+
+        [
+            {text:"Sock says, \"there's a monster in my room,", x:DEFAULT_X, y:DEFAULT_Y, time:2},
+
+            {text:"and I know he'll try to eat me soon.\"", x:DEFAULT_X, y:DEFAULT_Y, time:6},
+            {text:"All is quiet from celing to floor.", x:DEFAULT_X, y:DEFAULT_Y, time:11},
+            {text:"Is there really a monster behind the door?", x:DEFAULT_X, y:DEFAULT_Y, time:15}
+        ],
+
+        [
+            {text:"Sock is as curious as can be,", x:DEFAULT_X, y:DEFAULT_Y, time:2},
+
+            {text:"so he hops out of bed to go and see.", x:DEFAULT_X, y:DEFAULT_Y, time:5},
+            {text:"He opens the close and turns on the light,", x:DEFAULT_X, y:DEFAULT_Y, time:10},
+            {text:"and there are no monsters anywhere in sight.", x:DEFAULT_X, y:DEFAULT_Y, time:20}
+        ],
+
+        [
+            {text:"Just to be sure Sock looks all around,", x:DEFAULT_X, y:DEFAULT_Y, time:5},
+
+            {text:"then he hops back in bed to sleep safe and sound.", x:DEFAULT_X, y:DEFAULT_Y, time:10},
+            {text:"\"I'm glad I checked for myself,\" says Sock.", x:DEFAULT_X, y:DEFAULT_Y, time:13},
+            {text:"\"Now I won't be afraid when it turns 8 o'clock. \"", x:DEFAULT_X, y:DEFAULT_Y, time:17}
+        ],
+
+        [
+            {text:"So when Sock is afraid of the dark at night,", x:DEFAULT_X, y:DEFAULT_Y, time:3},
+
+            {text:"he just checks his whole room when he turns on the light.", x:DEFAULT_X, y:DEFAULT_Y, time:7},
+            {text:"Now Sock can sleep without a care,", x:DEFAULT_X, y:DEFAULT_Y, time:11},
+            {text:"because he knows there are no monsters there.", x:DEFAULT_X, y:DEFAULT_Y, time:17}
+        ]
+    ]
 ]
--- www/js/main.js
+++ www/js/main.js
@@ -8,7 +8,7 @@ var current_story = -1;
 
 //---------- options
 var autoplay_enabled = true;
-var audio_enabled = true;
+var audio_enabled = false;
 var music_enabled = false;
 
 
@@ -16,314 +16,324 @@ var timer = null;
 var subIndex;
 
 Media.prototype.stop_audio = function() {
-    this.stop();
-    window.audio_ended = true;
+  this.stop();
+  window.audio_ended = true;
 }
 
 function successCallback() {
-    window.audio_ended = true;
+  window.audio_ended = true;
 
 }
 
 function playAudio(src) {
-    console.log(src)
-    window.audio_ended = false;
-    if (current_audio) {
-        current_audio.stop();
-    }
-    current_audio = new Media(src, successCallback);
-    current_audio.play();
+  window.audio_ended = false;
+  if (current_audio) {
+    current_audio.stop();
+  }
+  current_audio = new Media(src, successCallback);
+  current_audio.play();
 }
 
-function stopNarration(){
-    window.audio_ended = false;
-    if (timer != null) {
-        
-        clearInterval(timer);
-        timer = null;
-    }
-    
-    if (current_audio) {
-        current_audio.stop();
-    }
+function stopNarration() {
+  window.audio_ended = false;
+  if (timer != null) {
+
+    clearInterval(timer);
+    timer = null;
+  }
+
+  if (current_audio) {
+    current_audio.stop();
+  }
 }
 
 function playNarration(src) {
-    stopNarration();
-    current_audio = new Media(src, function() {
-            successCallback();
-        }, function() {
-            console.log("fail");
-        }
-    );
-    current_audio.play();
+  stopNarration();
+  current_audio = new Media(src, function() {
+      successCallback();
+    }, function() {
+      console.log("fail");
+    }
+  );
+  current_audio.play();
 
-   
-    timer = setInterval(function() {
-                    
-        if (current_audio != null) {
-            current_audio.getCurrentPosition(function(position) {
-                                             
-                                             console.log(position);
-                if (position == -1) {
-                                           
-                    clearInterval(timer);
-                    audioDidFinished();
-                }
-                else {
-                                             positionDidChanged(position);
-                }
-            });
+
+  timer = setInterval(function() {
+
+    if (current_audio != null) {
+      current_audio.getCurrentPosition(function(position) {
+
+        console.log(position);
+        if (position == -1) {
+
+          clearInterval(timer);
+          audioDidFinished();
         }
         else {
-            clearInterval(timer);
+          positionDidChanged(position);
         }
-    }, 1000);
+      });
+    }
+    else {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
 
 /*
-function with_audio() {
-    current_audio.stop_audio();
-    playAudio('audio/1-1.wav');
-    autoplay = true;
-    pageMove();
-}
+ function with_audio() {
+ current_audio.stop_audio();
+ playAudio('audio/1-1.wav');
+ autoplay = true;
+ pageMove();
+ }
 
 
-function without_audio() {
-    current_audio.stop_audio();
-    autoplay = false;
-    pageMove();
-}*/
+ function without_audio() {
+ current_audio.stop_audio();
+ autoplay = false;
+ pageMove();
+ }*/
 
 function play_clip(object) {
-    //Check if this object placed on the current page
-    if (!event.target.id.match(new RegExp('area' + current_page + '_'))) return;
+  //Check if this object placed on the current page
+  if (!event.target.id.match(new RegExp('area' + current_page + '_'))) return;
 
-    current_audio.getCurrentPosition(function(position) {
-        if (position == -1) {
-            playAudio('audio/' + current_page + '-' + object + '.wav');
-        }
-    });
+  current_audio.getCurrentPosition(function(position) {
+    if (position == -1) {
+      playAudio('audio/' + current_page + '-' + object + '.wav');
+    }
+  });
 }
 
 
 function animalMove() {
-    var obj = document.getElementById("figcaption");
-    addClass(obj, "lizmove");
-    setTimeout(function() {
-        removeClass(obj, "lizmove");
-    }, 1000);
+  var obj = document.getElementById("figcaption");
+  addClass(obj, "lizmove");
+  setTimeout(function() {
+    removeClass(obj, "lizmove");
+  }, 1000);
 }
 
 
 //------------------------------------------
 
 function init() {
-    var w = $(window).width();
-    var h = $(window).height();
-    screen = {width:w, height:h};
+  var w = $(window).width();
+  var h = $(window).height();
+  screen = {width:w, height:h};
 
-    selectStory(1);
+  selectStory(1);
 
-    $("#auto_play_link").click(function() {
-                               console.log("play link click");
-    });
+  $("#auto_play_link").click(function() {
+    console.log("play link click");
+  });
 
-    $("#story_board_link").click(function() {
-        prevSub();
-    });
-    $("#main_menu_link").click(function() {
-        nextSub();
-    });
-
-
-    $(".subs span .txt").live("click", function(e) {
-
-        (e.offsetX > e.currentTarget.clientWidth / 2) ? nextSub() : prevSub();
+  $("#story_board_link").click(function() {
+    prevSub();
+  });
+  $("#main_menu_link").click(function() {
+    nextSub();
+  });
 
 
-    });
+  $(".subs span .txt").live("click", function(e) {
+
+    (e.offsetX > e.currentTarget.clientWidth / 2) ? nextSub() : prevSub();
+
+
+  });
 }
-
 
 
 function selectStory(index) {
-    if (index != current_story) {
-        removeStory();
-        
-        current_story = index;
+  if (index != current_story) {
+    removeStory();
 
-        setupPages( function(){
-           initFlip(screen.width, screen.height);
-           setupSubs();
-           setPage(0);         
-        } );
- 
-    }
+    current_story = index;
+
+    setupPages(function() {
+      initFlip(screen.width, screen.height);
+      setupSubs();
+      setPage(0);
+    });
+
+  }
 }
 
-function removeStory(){
+function removeStory() {
   stopFlip();
   $("#pages").remove();
 }
 
 function removeSubs() {
-    $(".subs").remove();
+  $(".subs").remove();
 }
 
 function hideSubs() {
-    $(_currentSub()).find("span").hide();
+  $(_currentSub()).find("span").hide();
 }
 
 function showSubs() {
-    $(_currentSub()).find("span:first").show();
+  $(_currentSub()).find("span:first").show();
 }
 
 function setPage(p) {
-    hideSubs();
-    current_page = p;
-    subIndex=0;
-    showSubs();
-    if (audio_enabled) {
-        playNarration(_getAudioPath());
-    }
+  hideSubs();
+  current_page = p;
+  subIndex = 0;
+  showSubs();
+  if (audio_enabled) {
+    playNarration(_getAudioPath());
+  }
 }
 
-function  setupPages(pagesLoadedHandler){
-    var pages = $("<div id='pages'/>");
-   
-    
-    console.log("loading....");
-    var imagesLoadedCount = 0;
-    var images = new Array();
-    for(var i=0; i<TEXTS[current_story].length; i++){
-      images[i] = new Image(); 
-      images[i].src= "stories/"+(current_story+1)+"/images/"+(i+1)+".jpg"; 
-      images[i].onload = function() { 
-          imagesLoadedCount++;
-          if (imagesLoadedCount==TEXTS[current_story].length){
-              console.log("start create dom");
-              console.log("stop loading....");
-              
-              for(var i=0; i<TEXTS[current_story].length; i++){
-                  var page = $("<div id='"+_pageId(i)+"' />").css("background-image", "url(stories/"+(current_story+1)+"/images/"+(i+1)+".jpg)");
-                  $(pages).append($("<section />").append(page)); 
-              }
-              $("#pageflip-canvas").after(pages);    
-              
+function setupPages(pagesLoadedHandler) {
+  var pages = $("<div id='pages'/>");
 
-              pagesLoadedHandler();
-              
-          }
-      
-      }; 
-    }
+  var imagesLoadedCount = 0;
+  var images = new Array();
+  for (var i = 0; i < TEXTS[current_story].length; i++) {
+    images[i] = new Image();
+    images[i].src = _pageImage(i);
+    images[i].onload = function() {
+      imagesLoadedCount++;
+      if (imagesLoadedCount == TEXTS[current_story].length) {
+
+
+        for (var i = 0; i < TEXTS[current_story].length; i++) {
+          var page = $("<div id='" + _pageId(i) + "' />").css("background-image", "url(" + _pageImage(i) + ")");
+          $(pages).append($("<section />").append(page));
+        }
+        $("#pageflip-canvas").after(pages);
+
+
+        pagesLoadedHandler();
+
+      }
+
+    };
+  }
 
 }
 
 function setupSubs() {
-    removeSubs();
-    var texts = TEXTS[current_story];
-    for (var i = 0; i < texts.length; i++) {
-        var ul = $("<span />").addClass('subs');
-        var pageTexts = texts[i];
-        for (var j = 0; j < pageTexts.length; j++) {
-            var sub = pageTexts[j];
-            var subEl = $("<span />");
-            var x = sub.x * screen.width / 100;
-            var y = sub.y * screen.height / 100;
+  removeSubs();
+  var texts = TEXTS[current_story];
+  for (var i = 0; i < texts.length; i++) {
+    var ul = $("<span />").addClass('subs');
+    var pageTexts = texts[i];
+    for (var j = 0; j < pageTexts.length; j++) {
+      var sub = pageTexts[j];
+      var subEl = $("<span />");
+      var x = sub.x * screen.width / 100;
+      var y = sub.y * screen.height / 100;
 
-            $(subEl).css("left", x + "px").css("top", y + "px");
+      $(subEl).css("left", x + "px").css("top", y + "px");
 
-            $(subEl).append($("<div />").html(sub.text).addClass('txt'));
-            $(ul).append(subEl);
-        }
-        $("body").append(ul);
+      $(subEl).append($("<div />").html(sub.text).addClass('txt'));
+      $(ul).append(subEl);
     }
+    $("body").append(ul);
+  }
 }
 
 
 function showOptions() {
-    $("#options").dialog();
+  $("#options").dialog();
 }
 
 function toggleMenu() {
-    $("#menu").toggle();
-    $("#nav").toggle();
-    var dir = ($("#menu").is(":visible")) ? "-=" : "+=";
-    var delta  = dir+$("#menu").height()+"px";
-    $(".subs span").animate({top: delta}, 1000);
+  var dir;
+
+  if ($("#menu").css("opacity")!=0) {
+    $("#menu").animate({opacity:0}, 1000);  
+    $("#nav a").animate({opacity:0}, 1000);
+    dir = "+=";
+  }
+  else {
+    $("#menu").animate({opacity:1}, 1000);
+    $("#nav a").animate({opacity:1}, 1000);
+    dir = "-=";
+  }
+
+  var delta = dir + $("#menu").height() + "px";
+  $(".subs span").animate({top:delta}, 1000);
 }
 
 function nextSub() {
-    var cur_sub = $(_currentSub()).find("span:visible:first");
-    var next_sub = $(cur_sub).next();
-    if (next_sub.length > 0) {
-        subIndex++;
-        $(cur_sub).hide();
-        $(next_sub).show();
-    }
+  var cur_sub = $(_currentSub()).find("span:visible:first");
+  var next_sub = $(cur_sub).next();
+  if (next_sub.length > 0) {
+    subIndex++;
+    $(cur_sub).hide();
+    $(next_sub).show();
+  }
 }
 
 function prevSub() {
-    var cur_sub = $(_currentSub()).find("span:visible:first");
-    var next_sub = $(cur_sub).prev();
-    if (next_sub.length > 0) {
-        subIndex--;
-        $(cur_sub).hide();
-        $(next_sub).show();
-    }
+  var cur_sub = $(_currentSub()).find("span:visible:first");
+  var next_sub = $(cur_sub).prev();
+  if (next_sub.length > 0) {
+    subIndex--;
+    $(cur_sub).hide();
+    $(next_sub).show();
+  }
 }
 
 function prevPage() {
-    stopNarration();
-    ToPrevPage();
+  stopNarration();
+  ToPrevPage();
 }
 
-function nextPage() {    
-    stopNarration();
-    ToNextPage();
+function nextPage() {
+  stopNarration();
+  ToNextPage();
 }
 
 
 function pageDidChanged(p) {
-    if (p != current_page) {
-        setPage(p);
-    }
+  if (p != current_page) {
+    setPage(p);
+  }
 }
 
 function audioDidFinished() {
-    console.log("audio finished");
-    if (autoplay_enabled) {
-        nextPage();
-    }
+  console.log("audio finished");
+  if (autoplay_enabled) {
+    nextPage();
+  }
 }
 
-function positionDidChanged(position){
-    
-    if (subIndex<0 || subIndex >= TEXTS[current_story][current_page].length) return;
-    console.log(subIndex)
-    console.log("compare "+position + " and "+ TEXTS[current_story][current_page][subIndex].time);
-    console.log(position>=TEXTS[current_story][current_page][subIndex].time);
-    if (position>=TEXTS[current_story][current_page][subIndex].time){
-        console.log("move" + position);
-        nextSub();
-    }
+function positionDidChanged(position) {
+  var pageSubs = TEXTS[current_story][current_page];
+  if (subIndex < 0 || subIndex >= pageSubs.length) return;
+  if (position >= pageSubs[subIndex].time) {
+    nextSub();
+  }
 }
 
 
 function _getAudioPath() {
-    var res = 'stories/' + (current_story + 1) + '/audio/' + (current_page + 1) + '.wav';
-    console.log(res);
-    return res;
+  var res = 'stories/' + (current_story + 1) + '/audio/' + (current_page + 1) + '.wav';
+  console.log(res);
+  return res;
 }
 
 function _currentSub() {
-    return $(".subs:nth(" + current_page + ")");
+  return $(".subs:nth(" + current_page + ")");
 
 }
 
 function _pageId(index) {
-    return "page_"+current_story+"_" + index;
+  return "page_" + current_story + "_" + index;
+}
+
+function _pageImage(index) {
+  return "stories/" + (current_story + 1) + "/images/" + (index + 1) + ".jpg";
+}
+
+function _device() {
+  if (screen.width == 1024) return "ipad";
+  else return "iphone";
 }
