902437a58fe728532d3cdbd33fbaa87643c29095
Alexey Kisel <aki@jetthoughts.com>
2012-01-19 01:02:52 +0200

    add story menu

--- www/js/menu.js
+++ www/js/menu.js
@@ -2,8 +2,43 @@ $(document).ready(function() {
   $("div.hproducts").coverflow();
   $(".media > a").live("click", function(){
                        $("#menu_area").hide();
-                       $("#pages_area").show();
+                       $("#story_area").show();
                        selectStory(parseInt($(this).attr("rel")));
                        return false;
                   });
+     
+        $("#m_story_board_link").live("click", function(){
+            $("#story_area").hide();
+            $("#menu_area").show();
+             removeStory();
+            return false;
+        });
+                  
+        $("#with_audio_link").live("click", function(){
+                                   console.log("with_audio_link");
+                                   $("#story_area").hide();
+                                   $("#pages_area").show();
+                                   audio_enabled = true;
+                                   setPage(0); 
+            return false;
+        });
+        $("#without_audio_link").live("click", function(){
+                                             console.log("without_audio_link");
+                                    $("#story_area").hide();
+                                      $("#pages_area").show();
+                                    audio_enabled = false;
+                                      setPage(0);  
+                                             return false;
+        });
+                  
+                  $("#auto_play_link").live("click", function(){
+                                                console.log("without_audio_link");
+                                                $("#story_area").hide();
+                                                $("#pages_area").show();
+                                                audio_enabled = true;
+                                                autoplay_link = true;
+                                                setPage(0);  
+                                                return false;
+                });    
+                  
 });
