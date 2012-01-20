14df9f8107e08b7d1506d9bcbff4d7b8f49d2005
Alexey Kisel <aki@jetthoughts.com>
2012-01-20 10:40:43 +0200

    phonegap prepare scripts

---
+++ prepare_android.sh
@@ -0,0 +1,2 @@
+mkdir ./assets/www/active_phonegap/
+cp -R ./assets_src/android/*.js ./assets/www/js/active_phonegap/
---
+++ prepare_ios.sh
@@ -0,0 +1,2 @@
+mkdir ./assets/www/active_phonegap/
+cp -R ./assets_src/ios/*.js ./assets/www/js/active_phonegap/
