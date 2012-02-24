662c320668914825bbbe376be22c11b056c7c14f
Alexey Kisel <aki@jetthoughts.com>
2012-02-24 17:42:47 +0200

    project for spanish

---
+++ AndroidEs/src/com/jetthoughts/socknboots_es/MainFrame.java
@@ -0,0 +1,22 @@
+package com.jetthoughts.socknboots_es;
+
+import android.os.Bundle;
+import android.view.View;
+import com.phonegap.*;
+
+public class MainFrame extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index_android.html");
+        super.appView.setVerticalScrollBarEnabled(false);
+        super.appView.setVerticalScrollbarOverlay(false);
+        super.appView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                    WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);*/
+    }
+}
+
